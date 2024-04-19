c44ff4d3f51e08012217b1bce556e22e927d5f89
Eric S <esimonds09@gmail.com>
2024-04-19 15:02:55 -0400

    Add function to start game over

--- game.js
+++ game.js
@@ -13,6 +13,32 @@ let userClickedPattern = [];
 // Button colors available for selection
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+// Capture the user clicked button
+$(".btn").click(function (e) {
+  let colorClicked = e.target.classList[1];
+  // Add selected color to array
+  userClickedPattern.push(colorClicked);
+  // Animate button
+  animatePress(colorClicked);
+  //Call to playSound function
+  playSound(colorClicked);
+  let colorIndex = userClickedPattern.length - 1;
+  // Send index to check answer
+  checkAnswer(colorIndex);
+});
+
+// Button click to begin the game
+$(document).keypress(function () {
+  if (!gameStarted) {
+    // Call nextSequence to choose color
+    nextSequence();
+    // Change heading to indicate the current level
+    $("h1").text(`Level ${level}`);
+    // Indicate game has started
+    gameStarted = true;
+  }
+});
+
 // Function to choose next game color
 function nextSequence() {
   // Increase the level
@@ -25,13 +51,12 @@ function nextSequence() {
   let randomChosenColor = buttonColors[randomNumber];
   // Adds the random color to the array
   gamePattern.push(randomChosenColor);
-
   // Call to playSound function
   playSound(randomChosenColor);
-
   // Create a button flash effect for chosen button color
   $(`#${randomChosenColor}`).fadeIn(50).fadeOut(50).fadeIn(50);
 }
+
 // Create function to play sounds
 function playSound(name) {
   // Create an audio for selected button color
@@ -61,31 +86,22 @@ function checkAnswer(currentLevel) {
     }
   } else {
     console.log("Failure");
+    // Play game over sound
+    playSound("wrong");
+    // Add game over effect to the game background
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game over. Press any key to restart!");
+    startOver();
   }
 }
 
-// Capture the user clicked button
-$(".btn").click(function (e) {
-  let colorClicked = e.target.classList[1];
-  // Add selected color to array
-  userClickedPattern.push(colorClicked);
-  // Animate button
-  animatePress(colorClicked);
-  //Call to playSound function
-  playSound(colorClicked);
-  let colorIndex = userClickedPattern.length - 1;
-  // Send index to check answer
-  checkAnswer(colorIndex);
-});
-
-// Button click to begin the game
-$(document).keypress(function () {
-  if (!gameStarted) {
-    // Call nextSequence to choose color
-    nextSequence();
-    // Change heading to indicate the current level
-    $("h1").text(`Level ${level}`);
-    // Indicate game has started
-    gameStarted = true;
-  }
-});
+// Reset the game variables
+function startOver() {
+  userClickedPattern = [];
+  gamePattern = [];
+  gameStarted = false;
+  level = 0;
+}
