9c6c8a39d5cda7e67e3cd03e4ddb5e3efa101038
Eric S <esimonds09@gmail.com>
2024-04-19 14:13:37 -0400

    Refactor to create playSound function

--- game.js
+++ game.js
@@ -16,11 +16,16 @@ function nextSequence() {
   // Adds the random color to the array
   gamePattern.push(randomChosenColor);
 
+  // Call to playSound function
+  playSound(randomChosenColor);
+
   // Create a button flash effect for chosen button color
   $(`#${randomChosenColor}`).fadeIn(50).fadeOut(50).fadeIn(50);
-
+}
+// Create function to play sounds
+function playSound(name) {
   // Create an audio for selected button color
-  let buttonAudio = new Audio(`./sounds/${randomChosenColor}.mp3`);
+  let buttonAudio = new Audio(`./sounds/${name}.mp3`);
   // Play audio
   buttonAudio.play();
 }
@@ -30,4 +35,6 @@ $(".btn").click(function (e) {
   let colorClicked = e.target.classList[1];
   // Add selected color to array
   userClickedPattern.push(colorClicked);
+  //Call to playSound function
+  playSound(colorClicked);
 });
